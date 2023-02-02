c2a4003382973e96dded65d86d01b442b76990c9
Appo <103305611+0xAppo@users.noreply.github.com>
2023-02-01 22:37:34 -0500

    removing unused contracts

--- contracts/Oracle/Interfaces/GLPManagerInterface.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BSD-3-Clause
-
-pragma solidity ^0.8.10;
-
-interface GLPManagerInterface {
-    function getAum(bool maximise) external view returns (uint256);
-
-    function getPrice(bool maximise) external view returns (uint256);
-}
