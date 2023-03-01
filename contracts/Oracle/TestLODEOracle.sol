cfca1ae275d023a02198798bbcb24b2a1f646776
Appo <103305611+0xAppo@users.noreply.github.com>
2023-02-28 20:03:51 -0500

    integrate with new oracle contract

--- contracts/Oracle/TestLODEOracle.sol
+++ contracts/Oracle/TestLODEOracle.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.10;
 
 contract TestLODEOracle {
+    bool public constant isSushiOracle = true;
+
     function price() public view returns (uint256) {
         uint256 price = 112693833393436;
         return price;
