3082e16c25491aedd50d72b4aebfb7d4e600886c
rotcivegaf <victor.fage@ripio.com>
2023-03-19 21:26:07 -0300

    improve gas

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -5,18 +5,18 @@ pragma solidity 0.8.10;
 import "./OpenZeppelin/Ownable.sol";
 
 contract Whitelist is Ownable {
-    constructor() payable {}
+    mapping(address => bool) private _isWhitelisted;
 
-    mapping(address => bool) public isWhitelisted;
+    constructor() payable { }
 
     function updateWhitelist(
         address _address,
         bool _isActive
-    ) external onlyOwner {
-        isWhitelisted[_address] = _isActive;
+    ) external payable onlyOwner {
+        _isWhitelisted[_address] = _isActive;
     }
 
     function getWhitelisted(address _address) external view returns (bool) {
-        return isWhitelisted[_address];
+        return _isWhitelisted[_address];
     }
 }
