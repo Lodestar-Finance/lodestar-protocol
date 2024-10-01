b565ec443c087ca5cfd80fbc49c54157593f7a42
Appo <103305611+0xAppo@users.noreply.github.com>
2024-09-30 23:01:50 -0400

    adding events and requirements to emissions, added IPOPE

--- contracts/EmissionModule.sol
+++ contracts/EmissionModule.sol
@@ -7,18 +7,29 @@ import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+import "./ComptrollerInterface.sol";
+import "./Oracle/Interfaces/IPOPE.sol";
+
 contract EmissionsModule is Ownable2Step, Pausable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    address public comptroller;
+    ComptrollerInterface public comptroller;
     address public emissionsHandler;
+    IPOPE public oracle;
 
     uint256 public pendingFees;
 
     IERC20 public emissionsToken;
     IERC20 public feeToken;
 
-    //add events
+    event ComptrollerSet(address indexed comptroller);
+    event EmissionsHandlerSet(address indexed emissionsHandler);
+    event EmissionsTokenSet(address indexed emissionsToken);
+    event FeeTokenSet(address indexed feeToken);
+    event OracleSet(address indexed oracle);
+
+    event Claimed(address indexed account, uint256 amount);
+    event FeesHandled(uint256 amount);
 
     constructor() Ownable() {}
 
@@ -34,7 +45,7 @@ contract EmissionsModule is Ownable2Step, Pausable, ReentrancyGuard {
         uint256 amount = getClaimable(msg.sender);
         require(amount > 0, "No claimable emissions");
 
-        IERC20(comptroller).safeTransfer(msg.sender, amount);
+        address(comptroller).transfer(msg.sender, amount);
     }
 
     function handleFees() external nonReentrant whenNotPaused {
@@ -44,27 +55,43 @@ contract EmissionsModule is Ownable2Step, Pausable, ReentrancyGuard {
     //** ADMIN FUNCTIONS */
 
     function setComptroller(address comptroller_) external onlyOwner {
+        require(comptroller_ != address(0), "Invalid comptroller");
+        //any other comptroller sanity checks?
         comptroller = comptroller_;
+        //if the unitroller changes, we want to also make sure we still have the correct oracle
+        IPOPE pendingOracle = comptroller.oracle();
+        //run some sanity checks here? Get price of protocol token and make sure its not 0?
+        oracle = pendingOracle;
+        emit ComptrollerSet(comptroller_);
+        emit OracleSet(oracle);
     }
 
     function setEmissionsHandler(address emissionsHandler_) external onlyOwner {
+        require(emissionsHandler_ != address(0), "Invalid emissions handler");
         emissionsHandler = emissionsHandler_;
+        emit EmissionsHandlerSet(emissionsHandler_);
     }
 
     function setEmissionsToken(IERC20 emissionsToken_) external onlyOwner {
+        require(address(emissionsToken_) != address(0), "Invalid emissions token");
         emissionsToken = emissionsToken_;
+        emit EmissionsTokenSet(address(emissionsToken_));
     }
 
     function setFeeToken(IERC20 feeToken_) external onlyOwner {
+        require(address(feeToken_) != address(0), "Invalid fee token");
         feeToken = feeToken_;
+        emit FeeTokenSet(address(feeToken_));
     }
 
     function pause() external onlyOwner {
         _pause();
+        emit Paused();
     }
 
     function unpause() external onlyOwner {
         _unpause();
+        emit Unpaused();
     }
 
     function withdraw(address token_, uint256 amount_) external onlyOwner {
