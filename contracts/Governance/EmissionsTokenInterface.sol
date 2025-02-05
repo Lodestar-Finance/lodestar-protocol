d2c00215bc18a8e51ddab897a1d422d08d3ea053
Appo <103305611+0xAppo@users.noreply.github.com>
2025-02-05 00:17:16 -0500

    removing remaining emissions module contracts

--- contracts/Governance/EmissionsTokenInterface.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-
-import "../../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface EmissionsTokenInterface is IERC20 {
-    function burn(uint256 amount) external;
-}
